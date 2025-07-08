337f778707a6ecb090426073a75ba3220d277613
GXAXVXO <gxaxvxo@gmail.com>
2025-07-07 21:26:11 -0500

    fix :bug: apikey

--- src/api/apiConfig2.ts
+++ src/api/apiConfig2.ts
@@ -9,6 +9,6 @@ export const apiClientFotMob = axios.create({
     Accept: "application/json",
     "key":"user-agent","value":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36",
     "x-client-version":"production:318f308489abf9e1a630afdc6389b7535860e6fa-undefined",
-    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9tYXRjaGVzP2RhdGU9MjAyNTA3MDYmdGltZXpvbmU9QW1lcmljYSUyRkxpbWEmY2NvZGUzPVBFUiIsImNvZGUiOjE3NTE4NTI5OTAzOTQsImZvbyI6InByb2R1Y3Rpb246MDBmN2Y1YmNkZmQ4M2I2ZjZhYjFlZDg0MTNmM2U5ZDI3MWZhNzQ0Zi11bmRlZmluZWQifSwic2lnbmF0dXJlIjoiMzY3NjE4NENFMzk5MTRCRUZCQkFEODJDRjg2Mjg2QzgifQ=="
+    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9hbGxMZWFndWVzP2xvY2FsZT1lcyZjb3VudHJ5PVBFUiIsImNvZGUiOjE3NTE5NDE0NzQ5MzQsImZvbyI6InByb2R1Y3Rpb246MDBmN2Y1YmNkZmQ4M2I2ZjZhYjFlZDg0MTNmM2U5ZDI3MWZhNzQ0Zi11bmRlZmluZWQifSwic2lnbmF0dXJlIjoiMzc3M0I1MzhFOTc5RTdFRjVBRUI5ODI1MUJDOEEzNDYifQ=="
   },
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { ThemeProvider } from "@/components/theme-provider"
-import ClientOnly from "@/components/clientonly"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -36,22 +35,19 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ClientOnly>
-            <SidebarProvider
-              style={
-                {
-                  "--sidebar-width": "calc(var(--spacing) * 72)",
-                  "--header-height": "calc(var(--spacing) * 12)",
-                } as React.CSSProperties
-              }
-            >
-              <AppSidebar variant="inset" />
-              <SidebarInset>
-                {children}
-              </SidebarInset>
-            </SidebarProvider>
-          </ClientOnly>
-
+          <SidebarProvider
+            style={
+              {
+                "--sidebar-width": "calc(var(--spacing) * 72)",
+                "--header-height": "calc(var(--spacing) * 12)",
+              } as React.CSSProperties
+            }
+          >
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+              {children}
+            </SidebarInset>
+          </SidebarProvider>
         </ThemeProvider>
 
 
