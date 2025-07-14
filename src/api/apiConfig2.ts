68271efbd9d08ac943114b12ce03d5b3c4eb9700
GXAXVXO <gxaxvxo@gmail.com>
2025-07-14 10:18:17 -0500

    fix :bug: apikey}

--- src/api/apiConfig2.ts
+++ src/api/apiConfig2.ts
@@ -9,6 +9,6 @@ export const apiClientFotMob = axios.create({
     Accept: "application/json",
     "key":"user-agent","value":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36",
     "x-client-version":"production:318f308489abf9e1a630afdc6389b7535860e6fa-undefined",
-    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9hbGxMZWFndWVzP2xvY2FsZT1lcyZjb3VudHJ5PVBFUiIsImNvZGUiOjE3NTE5NDE0NzQ5MzQsImZvbyI6InByb2R1Y3Rpb246MDBmN2Y1YmNkZmQ4M2I2ZjZhYjFlZDg0MTNmM2U5ZDI3MWZhNzQ0Zi11bmRlZmluZWQifSwic2lnbmF0dXJlIjoiMzc3M0I1MzhFOTc5RTdFRjVBRUI5ODI1MUJDOEEzNDYifQ=="
+    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9hdWRpby1tYXRjaGVzIiwiY29kZSI6MTc1MjUwNjEyOTYzMCwiZm9vIjoicHJvZHVjdGlvbjpkMTMxZTgxYTY3MjAwZTk0MTdjZTI1NDJkOGU3NGI1NTM5MWQ3OWYzLXVuZGVmaW5lZCJ9LCJzaWduYXR1cmUiOiIxOEY2NTAyNjdFRTZFNUU5NzE4MjM4QkFDQzdDQUQ0RSJ9"
   },
 });
