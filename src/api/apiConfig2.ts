74a18696d57d62c5b88586b6cdc5bb36e5af9da4
WHYTEROZE <WHXTXRXSX@gmail.com>
2025-08-04 19:10:57 -0500

    X-mas testing

--- src/api/apiConfig2.ts
+++ src/api/apiConfig2.ts
@@ -9,6 +9,6 @@ export const apiClientFotMob = axios.create({
     Accept: "application/json",
     "key":"user-agent","value":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36",
     "x-client-version":"production:318f308489abf9e1a630afdc6389b7535860e6fa-undefined",
-    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9tYXRjaGVzP2RhdGU9MjAyNTA3MTUmdGltZXpvbmU9QW1lcmljYSUyRkxpbWEmY2NvZGUzPVBFUiIsImNvZGUiOjE3NTI1OTM1MzY3NjgsImZvbyI6InByb2R1Y3Rpb246ZDEzMWU4MWE2NzIwMGU5NDE3Y2UyNTQyZDhlNzRiNTUzOTFkNzlmMy11bmRlZmluZWQifSwic2lnbmF0dXJlIjoiNjM5MDBFOEIxREM5RUI2MDhENTRBMzk1MTc0NkJCNkUifQ=="
+    "x-mas":"eyJib2R5Ijp7InVybCI6Ii9hcGkvZGF0YS9tYXRjaGVzP2RhdGU9MjAyNTA4MDQmdGltZXpvbmU9QW1lcmljYSUyRkxpbWEmY2NvZGUzPVBFUiIsImNvZGUiOjE3NTQzNTAyMTA0NzYsImZvbyI6InByb2R1Y3Rpb246OGJmYWYxZmJjODRhNWUxNTA2YTY3ZDE2ZWIxZGRmMzA4NjQ0OGQ0NS11bmRlZmluZWQifSwic2lnbmF0dXJlIjoiRkRFOTBBOEM3NUZFM0MwRjU4NDc2ODA1MzU1NzI2MTgifQ=="
   },
 });
